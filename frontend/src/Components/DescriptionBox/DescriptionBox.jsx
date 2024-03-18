ba6e28d7f90d55358071e198c09175b50f17c15d
Shobhit-Kumar-Yadav <143309281+Shobhit-Kumar-Yadav@users.noreply.github.com>
2024-03-19 01:47:14 +0530

    commit components

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e commerce website is an online platform that facilitattes the buying and selling of products or services over the internet.</p>
+            <p>An e commerce website is an online platform that facilitattes the buying and selling of products or services over the internet.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
